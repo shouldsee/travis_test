f4154edc516530cf772cb914dce4695d116757b2
shouldsee <shouldsee.gem@gmail.com>
2019-11-28 12:56:11 +0000

    Update travis_push.sh

--- travis_push.sh
+++ travis_push.sh
@@ -1,20 +1,31 @@
 #!/bin/sh
 
+GITHUB_BRANCH=${GITHUB_BRANCH:-gh-pages}
+GITHUB_TOKEN=${GITHUB_TOKEN}
+GITHUB_PATH=${GITHUB_PATH}
+
 setup_git() {
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "Travis CI"
 }
 
 commit_website_files() {
-  git checkout -b gh-pages
-  git add --all  
+  git checkout -b $GITHUB_BRANCH
+#   gh-pages
+#   find . | tee LOCAL_INDEX
+#   cat ../.gitignore
+#   git add ../ --all
+#   git status ../
 #  git add . *.html
+  git status
+  git add --all
   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 }
 
+
 upload_files() {
   git remote add origin-pages https://${GITHUB_TOKEN}@github.com/${GITHUB_PATH}.git > /dev/null 2>&1
-  git push --quiet --set-upstream origin-pages gh-pages --force
+  git push --quiet --set-upstream origin-pages ${GITHUB_BRANCH} --force
 }
 
 setup_git
